232e447c5118beaacdc7654440612e64dafb96dc
Alex <alessandro.piccione.75@gmail.com>
2026-05-06 20:01:47 +0200

    testL added Utils.fs to solve issue w=ith CI build

---
+++ src/UnitTests/Tools/Utils.fs
@@ -0,0 +1,31 @@
+﻿module Utils
+
+open System.IO
+open Tools.DirectoryExplorer.Models
+
+let dirSeparator = Path.DirectorySeparatorChar.ToString()
+
+let if_null (str: string|null) (defaultValue: string) =
+    match str with
+    | null -> defaultValue
+    | value -> value
+
+// NOTE. Path.GetDirectoryName returns the parent directory path, NOT the last directory of a path
+let getFilePath (path:string) = if_null (Path.GetFileName path) "not set"
+let getDirPath (path:string) =  if_null (Path.GetFileName path) "not set"
+    //let dirName = (path.Split "/")[..1]
+
+    //Path.GetDirectoryName
+    //if_null dirName "not set"
+
+type TreeItem with
+    static member File (path) = {Type="file"; Name=getFilePath path; Path=path}
+    static member Directory (path) = {Type="directory"; Name=getDirPath path; Path=path}
+
+
+let asWin (path:string) =
+    path.Replace("/", "\\")
+
+let asUnix (path:string) =
+    path.Replace("\\", "/")
+
